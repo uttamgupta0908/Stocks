94137a56087eb33aaaced19cf0f765bcf3290227
Uttam Gupta <uttamgupta0908@gmail.com>
2025-12-27 19:19:50 +0530

    feat: Introduce Watchlist, Product Details, Explore, and View All screens with new navigation, stock API, and UI components.

--- App.tsx
+++ App.tsx
@@ -1,28 +1,26 @@
 import React from 'react';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { WatchlistProvider } from './src/features/watchlist/context/WatchlistContext';
 
 import './global.css';
 import { RootNavigation } from './src/navigation/RootNavigation';
-
-import { useColorScheme } from 'nativewind';
-import { View, TouchableOpacity } from 'react-native';
+import { StatusBar } from 'react-native';
 
 const queryClient = new QueryClient();
 
 function App(): React.JSX.Element {
-  const { colorScheme, toggleColorScheme } = useColorScheme();
-
   return (
-    <SafeAreaProvider>
-      <QueryClientProvider client={queryClient}>
-        <WatchlistProvider>
-          <View className="flex-1 bg-background dark:bg-slate-900">
+
+    <SafeAreaProvider >
+      <SafeAreaView style={{ flex: 1 }}>
+        <QueryClientProvider client={queryClient}>
+          <WatchlistProvider>
+            <StatusBar translucent />
             <RootNavigation />
-          </View>
-        </WatchlistProvider>
-      </QueryClientProvider>
+          </WatchlistProvider>
+        </QueryClientProvider>
+      </SafeAreaView>
     </SafeAreaProvider>
   );
 }
